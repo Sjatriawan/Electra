a630c8651530f52e7a2da4abebfdbc0cbc8a368b
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-04-08 22:14:29 +0700

    fix separator line

--- Electra/Presentation/Kalkulasi/Kalkulasi.swift
+++ Electra/Presentation/Kalkulasi/Kalkulasi.swift
@@ -9,75 +9,106 @@ import SwiftUI
 
 struct Kalkulasi: View {
     var body: some View {
-//        NavigationView {
-//
-//        }
-        
-        VStack{
-//            NavigationView{
-//                Text("")
-//            }
-//            .navigationTitle("Kalkulasi")
-//            .navigationBarTitleDisplayMode(.inline)
-//            .toolbar{
-//                ToolbarItem(placement: .confirmationAction){
-//                    Button("Simpan"){
-//                        print("saved")
-//                    }
-//                }
-//            }
-//            Spacer()
-//                .frame(height: 20)
-            Grid(alignment: .leading,
-                 horizontalSpacing: 35,
-                 verticalSpacing: 44
-            ){
-                GridRow{
-                    Text("Nama Alat")
-                        .foregroundColor(Color("TextColor"))
-                        .font(.system(size: 17, weight: .medium))
-                    TextField("Nama alat elektronik", text: .constant(""))
-                        .font(.system(size: 17))
+        NavigationView {
+            VStack{
+                //            NavigationView{
+                //                Text("")
+                //            }
+                //            .navigationTitle("Kalkulasi")
+                //            .navigationBarTitleDisplayMode(.inline)
+                //            .toolbar{
+                //                ToolbarItem(placement: .confirmationAction){
+                //                    Button("Simpan"){
+                //                        print("saved")
+                //                    }
+                //                }
+                //            }
+                Spacer()
+                    .frame(height: 20)
+                Grid(alignment: .leading,
+                     horizontalSpacing: 35,
+                     verticalSpacing: 44
+                ){
+                    GridRow{
+                        Text("Nama Alat")
+                            .foregroundColor(Color("TextColor"))
+                            .font(.system(size: 15, weight: .medium))
+                        TextField("Nama alat elektronik", text: .constant(""))
+                            .font(.system(size: 15))
+                            .overlay{
+                                Divider()
+                                    .background(Color("textFieldLineSeparator"))
+                                    .offset(x: 0, y: 20)
+                                    .frame(width: 212)
+                            }
+                    }
+                    GridRow{
+                        Text("Jumlah")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Banyaknya alat elektronik", text: .constant(""))
+                            .font(.system(size: 15))
+                            .overlay{
+                                Divider()
+                                    .background(Color("textFieldLineSeparator"))
+                                    .offset(x: 0, y: 20)
+                                    .frame(width: 212)
+                            }
+                    }
+                    GridRow{
+                        Text("Beban Alat")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Beban (Watt/V/A)", text: .constant(""))
+                            .font(.system(size: 15))
+                            .overlay{
+                                Divider()
+                                    .background(Color("textFieldLineSeparator"))
+                                    .offset(x: 0, y: 20)
+                                    .frame(width: 212)
+                            }
+                    }
+                    GridRow{
+                        Text("Waktu")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Pemakaian (jam/hari)", text: .constant(""))
+                            .font(.system(size: 15))
+                            .overlay{
+                                Divider()
+                                    .background(Color("textFieldLineSeparator"))
+                                    .offset(x: 0, y: 20)
+                                    .frame(width: 212)
+                            }
+                    }
+                    GridRow{
+                        Text("Hari")
+                            .font(.system(size: 15, weight: .medium))
+                            .foregroundColor(Color("TextColor"))
+                        TextField("Setiap hari/tertentu", text: .constant(""))
+                            .font(.system(size: 15))
+                            .overlay{
+                                Divider()
+                                    .background(Color("textFieldLineSeparator"))
+                                    .offset(x: 0, y: 20)
+                                    .frame(width: 212)
+                            }
+                    }
                 }
-//                Divider()
-//
-//                    .frame(width: 212, height: 1, alignment: .trailing)
-                GridRow{
-                    Text("Jumlah")
-                        .font(.system(size: 17, weight: .medium))
-                        .foregroundColor(Color("TextColor"))
-                    TextField("Banyaknya alat elektronik", text: .constant(""))
-                        .font(.system(size: 17))
-                }
-                GridRow{
-                    Text("Beban Alat")
-                        .font(.system(size: 17, weight: .medium))
-                        .foregroundColor(Color("TextColor"))
-                    TextField("Beban (Watt/V/A)", text: .constant(""))
-                        .font(.system(size: 17))
-                    
-                }
-                GridRow{
-                    Text("Waktu")
-                        .font(.system(size: 17, weight: .medium))
-                        .foregroundColor(Color("TextColor"))
-                    TextField("Pemakaian (jam/hari)", text: .constant(""))
-                        .font(.system(size: 17))
-                    
-                }
-                GridRow{
-                    Text("Hari")
-                        .font(.system(size: 17, weight: .medium))
-                        .foregroundColor(Color("TextColor"))
-                    TextField("Setiap hari/tertentu", text: .constant(""))
-                        .font(.system(size: 17))
-                    
+                Spacer()
+            }
+            .padding(35)
+            .navigationTitle("Kalkulasi")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar{
+                ToolbarItem(placement: .confirmationAction){
+                    Button("Simpan"){
+                        print("saved")
+                    }
                 }
             }
-            Spacer()
         }
-        .padding(35)
-
+        
     }
 }
 
