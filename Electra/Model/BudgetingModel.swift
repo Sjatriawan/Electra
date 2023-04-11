257e9ebced1696c6c9a8d643becf97495970c710
Sjatriawan <aryadendi@gmail.com>
2023-04-11 23:57:25 +0700

    Add Model and Viewmode Budgeting

---
+++ Electra/Model/BudgetingModel.swift
@@ -0,0 +1,12 @@
+//
+//  BudgetData.swift
+//  Electra
+//
+//  Created by M Yogi Satriawan on 11/04/23.
+//
+import Foundation
+
+struct BudgetingModel: Codable {
+    var tarif: Double
+    var biaya: Double
+}
---
+++ Electra/ViewModel/BudgetViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetViewModel.swift
+//  Electra
+//
+//  Created by M Yogi Satriawan on 11/04/23.
+//
+
+import Foundation
+
+class BudgetingViewModel: ObservableObject {
+    @Published var budgetingList: [BudgetingModel] = []
+
+    func addBudgeting(tarif: Double, biaya: Double) {
+        let budgeting = BudgetingModel(tarif: tarif, biaya: biaya)
+        budgetingList.append(budgeting)
+    }
+}
